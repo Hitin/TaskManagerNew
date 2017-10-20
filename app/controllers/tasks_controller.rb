c7493bce2e03023580ec2349162a95af0aac1dac
Petr Ablyazov <phoenix-h@mail.ru>
2017-10-20 08:25:56 +0300

    Create model Task, controller Tasks, view index,show,create. Change route

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,29 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+
+  def create    
+    @task = Task.new(tasks_params)
+    @task.save
+    redirect_to @task 
+  end
+
+  private
+  def tasks_params
+    params.require(:task).permit(:title)
+  end
+end
