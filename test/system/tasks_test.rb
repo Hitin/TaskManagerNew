ab7d3e351ac4443ba62f98671e6963cea04a48b1
Petr Ablyazov <phoenix-h@mail.ru>
2017-10-27 10:22:01 +0300

    Add system test

--- test/system/tasks_test.rb
+++ test/system/tasks_test.rb
@@ -5,4 +5,15 @@ class TasksTest < ApplicationSystemTestCase
     visit tasks_url
     assert_selector 'h1', text: 'All tasks'
   end
+
+  test 'creating an task' do
+    visit tasks_path
+
+    click_on 'New Task'
+    fill_in 'task[title]', with: 'Creating an Task'
+
+    click_on 'Create Task'
+
+    assert_text 'Creating an Task'
+  end
 end
