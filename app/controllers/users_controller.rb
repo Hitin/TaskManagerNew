5282204b80be754ef0754cb9f64be502cd6e9433
Petr Ablyazov <phoenix-h@mail.ru>
2017-11-03 11:36:44 +0300

    Add tests users controller. Add users_controller. Views for index,show,new,create.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,31 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+  
   def show
     @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(users_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def users_params
+    params.require(:user).permit(:name)
   end
 end
